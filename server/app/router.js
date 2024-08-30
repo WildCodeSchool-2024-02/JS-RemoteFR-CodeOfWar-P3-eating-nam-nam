d0edc6d5a200dbe6901f741b021887250a889ebf
anaisdufourneau <anais.dufourneau87@gmail.com>
2024-08-30 11:26:43 +0200

    route, browse done !

--- server/app/router.js
+++ server/app/router.js
@@ -3,11 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 const itemsRouter = require("./router/itemsRouter");
-
+const ingredientsRouter = require("./router/ingredientsRouter");
 /* ************************************************************************* */
 // Define Your API Routes Here (/api)
 /* ************************************************************************* */
+/* ************************************************************************* */
 
+router.use("/ingredients", ingredientsRouter);
 router.use("/items", itemsRouter);
 
 /* ************************************************************************* */
--- server/app/router/ingredientsRouter.js
+++ server/app/router/ingredientsRouter.js
@@ -7,7 +7,5 @@ const ingredientActions = require("../controllers/ingredientActions");
 router.get("/", ingredientActions.browse);
 router.get("/:id", ingredientActions.read);
 router.post("/", ingredientActions.add);
-router.put("/:id", ingredientActions.edit);
-router.delete("/:id", ingredientActions.remove);
 
 module.exports = router;
