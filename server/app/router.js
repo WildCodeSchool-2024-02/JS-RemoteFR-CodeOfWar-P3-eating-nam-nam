da44e24c07d65e6fffe477e8b96da88f0b50ccc1
PixelKata <guillaumebasilaire@gmail.com>
2024-09-02 16:46:19 +0200

    :hammer: update

--- server/app/router.js
+++ server/app/router.js
@@ -3,12 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 const itemsRouter = require("./router/itemsRouter");
+const userRouter = require("./router/userRouter");
 
 /* ************************************************************************* */
 // Define Your API Routes Here (/api)
 /* ************************************************************************* */
 
 router.use("/items", itemsRouter);
+router.use("/user", userRouter);
 
 /* ************************************************************************* */
 
