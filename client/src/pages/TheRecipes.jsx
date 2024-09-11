f2a287f5dac0809deba9a63bffeb741b267897b5
Your Name <ewan16270409@outlook.fr>
2024-09-11 12:53:36 +0200

    Add dessert

--- client/src/pages/TheRecipes.jsx
+++ client/src/pages/TheRecipes.jsx
@@ -8,6 +8,7 @@ import bruschetta from "../assets/images/bruschetta.svg";
 import carpaccioDeBoeuf from "../assets/images/carpaccio_de_boeuf.svg";
 import saladeLyonnaise from "../assets/images/salade_lyonnaise.svg";
 import soupePotimarron from "../assets/images/soupe_potimarron.svg";
+
 import cheeseburger from "../assets/images/cheeseburger.svg";
 import galetteComplete from "../assets/images/galette_complete.svg";
 import makis from "../assets/images/makis.svg";
@@ -17,6 +18,16 @@ import porcAuCaramel from "../assets/images/porc_au_caramel.svg";
 import pouletAuCurry from "../assets/images/poulet_au_curry.svg";
 import pouletAuxOlives from "../assets/images/poulet_aux_olives.svg";
 import pureeSteak from "../assets/images/puree_steak.svg";
+
+import bananeCaramelisee from "../assets/images/banane_caramelisee.svg";
+import barreGlaceChocolat from "../assets/images/barre_glace_chocolat.svg";
+import compoteDePomme from "../assets/images/compote_de_pomme.svg";
+import crepeAbricot from "../assets/images/crepe_abricot.svg";
+import fraisesChantilly from "../assets/images/fraises_chantilly.svg";
+import gaufreSauceChocolat from "../assets/images/gaufre_sauce_chocolat.svg";
+import nougat from "../assets/images/nougat.svg";
+import pancakesSiropErable from "../assets/images/pancakes_sirop_erable.svg";
+import saladeDeFruits from "../assets/images/salade_de_fruits.svg";
 
 const entreeData = [
   { name: "Gazpacho de tomate", image: gazpachoTomate },
@@ -30,14 +41,26 @@ const entreeData = [
 
 const platData = [
   { name: "Cheeseburger", image: cheeseburger },
-  { name: "Galette complete", image: galetteComplete },
+  { name: "Galette complète", image: galetteComplete },
   { name: "Makis", image: makis },
   { name: "Pate bolognaise", image: pateBolognaise },
-  { name: "Poisson haricots puree", image: poissonHaricotsPuree },
+  { name: "Poisson haricots purée", image: poissonHaricotsPuree },
   { name: "Porc au caramel", image: porcAuCaramel },
   { name: "Poulet au curry", image: pouletAuCurry },
   { name: "Poulet aux olives", image: pouletAuxOlives },
   { name: "Puree steak", image: pureeSteak },
+];
+
+const dessertData = [
+  { name: "Banane caramelisée", image: bananeCaramelisee },
+  { name: "Compote de pomme", image: compoteDePomme },
+  { name: "Crepe Abricot", image: crepeAbricot },
+  { name: "Fraises chantilly", image: fraisesChantilly },
+  { name: "Gaufre sauce chocolat", image: gaufreSauceChocolat },
+  { name: "Nougat", image: nougat },
+  { name: "Pancakes sirop d'érable", image: pancakesSiropErable },
+  { name: "Salade de fruits", image: saladeDeFruits },
+  { name: "Barre glacé chocolat", image: barreGlaceChocolat },
 ];
 
 function TheRecipes() {
@@ -53,7 +76,7 @@ function TheRecipes() {
       </section>
       <section>
         <h2>LES DESSERTS</h2>
-        {/* Ajouter d'autres sections si nécessaire */}
+        <Carrousel items={dessertData} />
       </section>
     </div>
   );
