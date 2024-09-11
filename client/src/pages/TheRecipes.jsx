9f7e30a915af63a14918cb601f01599c3555bf68
Your Name <ewan16270409@outlook.fr>
2024-09-11 12:36:15 +0200

    Add plats

--- client/src/pages/TheRecipes.jsx
+++ client/src/pages/TheRecipes.jsx
@@ -8,6 +8,15 @@ import bruschetta from "../assets/images/bruschetta.svg";
 import carpaccioDeBoeuf from "../assets/images/carpaccio_de_boeuf.svg";
 import saladeLyonnaise from "../assets/images/salade_lyonnaise.svg";
 import soupePotimarron from "../assets/images/soupe_potimarron.svg";
+import cheeseburger from "../assets/images/cheeseburger.svg";
+import galetteComplete from "../assets/images/galette_complete.svg";
+import makis from "../assets/images/makis.svg";
+import pateBolognaise from "../assets/images/pate_bolognaise.svg";
+import poissonHaricotsPuree from "../assets/images/poisson_haricots_puree.svg";
+import porcAuCaramel from "../assets/images/porc_au_caramel.svg";
+import pouletAuCurry from "../assets/images/poulet_au_curry.svg";
+import pouletAuxOlives from "../assets/images/poulet_aux_olives.svg";
+import pureeSteak from "../assets/images/puree_steak.svg";
 
 const entreeData = [
   { name: "Gazpacho de tomate", image: gazpachoTomate },
@@ -19,16 +28,28 @@ const entreeData = [
   { name: "Soupe potimarron", image: soupePotimarron },
 ];
 
+const platData = [
+  { name: "Cheeseburger", image: cheeseburger },
+  { name: "Galette complete", image: galetteComplete },
+  { name: "Makis", image: makis },
+  { name: "Pate bolognaise", image: pateBolognaise },
+  { name: "Poisson haricots puree", image: poissonHaricotsPuree },
+  { name: "Porc au caramel", image: porcAuCaramel },
+  { name: "Poulet au curry", image: pouletAuCurry },
+  { name: "Poulet aux olives", image: pouletAuxOlives },
+  { name: "Puree steak", image: pureeSteak },
+];
+
 function TheRecipes() {
   return (
     <div>
-      <section className="Entree">
+      <section>
         <h2>LES ENTREES</h2>
         <Carrousel items={entreeData} />
       </section>
       <section>
         <h2>LES PLATS</h2>
-        {/* Ajouter d'autres sections si nécessaire */}
+        <Carrousel items={platData} />
       </section>
       <section>
         <h2>LES DESSERTS</h2>
