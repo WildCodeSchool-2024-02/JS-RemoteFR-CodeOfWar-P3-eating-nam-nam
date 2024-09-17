03ea97e84a4f5264e685438001a12e1d5bd7c9e4
anaisdufourneau <anais.dufourneau87@gmail.com>
2024-09-17 18:11:37 +0200

    :wrench: add link home send to recettes and link recettes to recipesInstruction

--- client/src/pages/RecipesInstruction.jsx
+++ client/src/pages/RecipesInstruction.jsx
@@ -86,63 +86,68 @@ export default function RecipesInstruction() {
           <div>3 feuilles de basilic</div>
         </div>
       </article>
-      <article className="Recette">
-        <h2>Recette</h2>
-        <div className="Etape-Global">
-          <div className="Etape">
-            <h3>Étape 1 :</h3>
-            <p>
-              Si besoin, faites décongeler les steaks hachés. Émincez l’échalote
-              finement.
-            </p>
-          </div>
-          <div className="Etape">
-            <h3>Étape 2 :</h3>
-            <p>
-              Dans un récipient ajoutez le boeuf, l’oeuf, la
-              chapelure,l’echalote, la moitié du parmesan, salez, poivrez,
-              malaxer le tout avec les mains.
-            </p>
-          </div>
-          <div className="Etape">
-            <h3>Étape 3 :</h3>
-            <p>
-              Faire des boulettes de 4 cm environ en les roulant entre vos
-              doigts.
-            </p>
-          </div>
-          <div className="Etape">
-            <h3>Étape 4 :</h3>
-            <p>
-              Cuire les pâtes selon les instructions du paquet dans une eau
-              salée.
-            </p>
-          </div>
-          <div className="Etape">
-            <h3>Étape 5 :</h3>
-            <p>
-              Faire revenir les boulettes 5 minutes à la poêle bien chaude avec
-              un filet d'huile d'olive.
-            </p>
-          </div>
-          <div className="Etape">
-            <h3>Étape 6 :</h3>
-            <p>
-              En fin de cuisson, baisser le feu au minimum, ajoutez la sauce
-              tomate, 1 cuillère à soupe d'eau/personne et laissez mijoter le
-              temps de la prochaine étape.
-            </p>
-          </div>
-          <div className="Etape">
-            <h3>Étape 7 :</h3>
-            <p>
-              Une fois les pâtes cuites, égouttez-les, puis mélangez-les avec la
-              sauce et les boulettes. Servir avec un peu de parmesan (optionnel
-              : et de basilic frais si vous en avez) sur le dessus. C'est prêt !
-            </p>
+
+      <article className="Recette-test">
+        <div className="Recette">
+          <h2>Recette</h2>
+          <div className="Etape-Global">
+            <div className="Etape">
+              <h3>Étape 1 :</h3>
+              <p>
+                Si besoin, faites décongeler les steaks hachés. Émincez
+                l’échalote finement.
+              </p>
+            </div>
+            <div className="Etape">
+              <h3>Étape 2 :</h3>
+              <p>
+                Dans un récipient ajoutez le boeuf, l’oeuf, la
+                chapelure,l’echalote, la moitié du parmesan, salez, poivrez,
+                malaxer le tout avec les mains.
+              </p>
+            </div>
+            <div className="Etape">
+              <h3>Étape 3 :</h3>
+              <p>
+                Faire des boulettes de 4 cm environ en les roulant entre vos
+                doigts.
+              </p>
+            </div>
+            <div className="Etape">
+              <h3>Étape 4 :</h3>
+              <p>
+                Cuire les pâtes selon les instructions du paquet dans une eau
+                salée.
+              </p>
+            </div>
+            <div className="Etape">
+              <h3>Étape 5 :</h3>
+              <p>
+                Faire revenir les boulettes 5 minutes à la poêle bien chaude
+                avec un filet d'huile d'olive.
+              </p>
+            </div>
+            <div className="Etape">
+              <h3>Étape 6 :</h3>
+              <p>
+                En fin de cuisson, baisser le feu au minimum, ajoutez la sauce
+                tomate, 1 cuillère à soupe d'eau/personne et laissez mijoter le
+                temps de la prochaine étape.
+              </p>
+            </div>
+            <div className="Etape">
+              <h3>Étape 7 :</h3>
+              <p>
+                Une fois les pâtes cuites, égouttez-les, puis mélangez-les avec
+                la sauce et les boulettes. Servir avec un peu de parmesan
+                (optionnel : et de basilic frais si vous en avez) sur le dessus.
+                C'est prêt !
+              </p>
+            </div>
           </div>
         </div>
       </article>
+
       <article className="Smiley-Langue">
         <h3>Bon Appétit</h3>
         <img src={smileyLangue} alt="Smiley qui tire la langue" />
