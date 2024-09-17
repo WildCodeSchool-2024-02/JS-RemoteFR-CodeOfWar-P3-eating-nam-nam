e427ad88333efececbbe65e430435525328fa99e
Benjamin <dilgopierre@gmail.com>
2024-09-17 10:52:35 +0200

    :adhesive_bandage: Adding useState to forms

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,25 +1,33 @@
+import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/Auth.css";
-import { useState } from "react";
 
 export default function Login() {
   const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
   const [error, setError] = useState("");
 
-  const handleSumbit = async (event) => {
-    event.preventDefault();
-    const email = event.target.elements.namedItem("email").value;
-    const password = event.target.elements.namedItem("pass").value;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     try {
       await axios.post(
         `${import.meta.env.VITE_API_URL}/api/auth/login`,
-        { email, password }
+        formData
       );
-
       setError("");
-      navigate("/")
+      navigate("/");
     } catch (err) {
       console.error("Erreur lors de la connexion : ", err);
       setError("La connexion a échoué. Vérifiez vos identifiants");
@@ -27,7 +35,7 @@ export default function Login() {
   };
 
   return (
-    <form className="auth" onSubmit={handleSumbit}>
+    <form className="auth" onSubmit={handleSubmit}>
       <h1>Connexion</h1>
       <div>
         <section>
@@ -37,21 +45,25 @@ export default function Login() {
             name="email"
             id="email"
             placeholder="Votre email..."
+            value={formData.email}
+            onChange={handleChange}
           />
         </section>
         <section>
           <label htmlFor="password">Mot de passe</label>
           <input
             type="password"
-            name="pass"
+            name="password"
             id="password"
             placeholder="Votre mot de passe..."
+            value={formData.password}
+            onChange={handleChange}
           />
         </section>
-        <p className="error">{error}</p>
+        {error && <p className="error">{error}</p>}
       </div>
       <button type="submit">Je me connecte</button>
-      <Link to="/connexion?forgot=true">J’ai oublié mon mot de passe...</Link>
+      <Link to="/connexion?forgot=true">J'ai oublié mon mot de passe...</Link>
     </form>
   );
 }
