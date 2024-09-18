1e7718ffe8512eb5538d6e61abe44b27bb0f23b5
Mathieu REY <mathieu.rey33@gmail.com>
2024-09-18 15:55:51 +0200

    :hammer: Front Buffet ok, et ajout des images necessaires dans assets

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,6 +14,7 @@ import SignUp from "./pages/SignUp";
 
 import PanelAdmin from "./pages/userAdmin/PanelAdmin";
 import User from "./pages/User";
+import LeBuffet from "./pages/leBuffet/LeBuffet";
 
 const router = createBrowserRouter([
   {
@@ -48,6 +49,10 @@ const router = createBrowserRouter([
       {
         path: "/user",
         element: <User />,
+      },
+      {
+        path: "/le-buffet",
+        element: <LeBuffet />,
       },
     ],
   },
