6449b44060d0b76941837c75cd6b398bf67d5bfb
PixelKata <guillaumebasilaire@gmail.com>
2024-09-20 15:16:11 +0200

    :hammer: Add upload to the backend

---
+++ server/app/router/uploadRouter.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+
+const path = require("path");
+
+const { add } = require("../controllers/uploadActions");
+const { verifyToken } = require("../services/auth");
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/upload/");
+  },
+  filename: (req, file, cb) => {
+    const id = uuidv4();
+    const filename = `${id}${path.extname(file.originalname)}`;
+    cb(null, filename);
+    req.body.filename = filename;
+  },
+});
+
+const upload = multer({ storage });
+
+router.post("/", verifyToken, upload.single("file"), add);
+
+router.use("/", verifyToken, express.static("public/upload/"));
+
+module.exports = router;
