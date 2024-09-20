0c70cd38839c1f9d55ef3c330a5ca1093f0e4f4f
Mathieu REY <mathieu.rey33@gmail.com>
2024-09-20 15:36:38 +0200

    :hammer: Add images , cards and CSS

--- client/src/pages/userAdmin/PanelAdminRecipes.jsx
+++ client/src/pages/userAdmin/PanelAdminRecipes.jsx
@@ -1,11 +1,27 @@
 import { useState } from "react";
 import "../../styles/panelAdmin/AdminRecipes.css";
+import entree1 from "../../assets/images/entree1.jpg";
+import entree2 from "../../assets/images/entree2.jpg";
+import entree3 from "../../assets/images/entree3.jpg";
+import plat1 from "../../assets/images/plat1.jpg";
+import plat2 from "../../assets/images/plat2.jpg";
+import plat3 from "../../assets/images/plat3.jpg";
+import dessert1 from "../../assets/images/dessert1.jpg";
+import dessert2 from "../../assets/images/dessert2.jpg";
+import dessert3 from "../../assets/images/dessert3.jpg";
 
 export default function AdminRecipe() {
   const [recipes, setRecipes] = useState([
-    { id: 1, title: "Lorem 1", image: "image1.jpg" },
-    { id: 2, title: "Lorem 2", image: "image2.jpg" },
-    { id: 3, title: "Lorem 3", image: "image3.jpg" },
+    { id: 1, title: "Tomate garnie", image: entree1 },
+    { id: 2, title: "Dahl lentilles corail", image: plat1 },
+    { id: 3, title: "Tiramisu", image: entree2 },
+    { id: 4, title: "Tiramisu", image: entree3 },
+    { id: 5, title: "Tiramisu", image: plat2 },
+    { id: 6, title: "Tiramisu", image: plat3 },
+    { id: 7, title: "Tiramisu", image: dessert2 },
+    { id: 8, title: "Tiramisu", image: dessert3 },
+    { id: 9, title: "Tiramisu", image: dessert1 },
+    { id: 10, title: "Tiramisu", image: dessert1 },
   ]);
 
   const handleDelete = (id) => {
@@ -14,26 +30,28 @@ export default function AdminRecipe() {
 
   return (
     <div className="recipe-AdminList">
-      <h1>Recettes</h1>
-      {recipes.map((recipe) => (
-        <div className="recipe-AdminCard" key={recipe.id}>
-          <img
-            src={recipe.image}
-            alt={recipe.title}
-            className="recipe-AdminImage"
-          />
-          <div className="recipe-details">
-            <p>{recipe.title}</p>
-            <button
-              onClick={() => handleDelete(recipe.id)}
-              className="delete-button"
-              type="button"
-            >
-              ❌
-            </button>
+      <h1 className="recipe-AdminTitle">Recettes</h1>
+      <div className="recipe-AdminCards">
+        {recipes.map((recipe) => (
+          <div className="recipe-AdminCard" key={recipe.id}>
+            <img
+              src={recipe.image}
+              alt={recipe.title}
+              className="recipe-AdminImage"
+            />
+            <div className="recipe-details">
+              <p>{recipe.title}</p>
+              <button
+                onClick={() => handleDelete(recipe.id)}
+                className="delete-button"
+                type="button"
+              >
+                ❌
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
       <button className="back-button" type="button">
         Retour
       </button>
