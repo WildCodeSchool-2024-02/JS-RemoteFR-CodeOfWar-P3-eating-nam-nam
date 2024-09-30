37c005bec78be454c9082896f108388ba6ad2ea6
Benjamin <dilgopierre@gmail.com>
2024-09-30 14:33:42 +0200

    :hammer: (Recipes) Add creator username and remove delete button for all user

--- client/src/pages/RecipesInstruction.jsx
+++ client/src/pages/RecipesInstruction.jsx
@@ -12,14 +12,14 @@ import four from "../assets/images/four.svg";
 import smileyLangue from "../assets/images/smiley_langue.svg";
 import heartRed from "../assets/images/heart-red.svg";
 import Comment from "../components/Comment";
+import { useAuth } from "../context/authContext";
 
 export default function RecipesInstruction() {
   const { recipe, comments: initialComments } = useLoaderData();
+  const { user } = useAuth();
   const navigate = useNavigate();
   const [commentData, setCommentData] = useState("");
   const [comments, setComments] = useState(initialComments);
-
-  console.info(comments);
 
   const stars = [1, 2, 3, 4, 5];
 
@@ -54,7 +54,7 @@ export default function RecipesInstruction() {
           <div className="Countain">
             <div className="Pseudo-Avatar">
               <img src={photoProfil} alt="profil de l'utilisateur" />
-              <p>CookDiary_anais</p>
+              <p>{recipe.creator_username}</p>
             </div>
             <div className="dishes">
               <img
@@ -85,13 +85,15 @@ export default function RecipesInstruction() {
             </div>
             <img src={heartRed} alt="coeur rouge" />
           </div>
-          <button
-            type="button"
-            className="delete-recipe-btn"
-            onClick={() => handleDeleteRecipe(recipe.id, navigate)}
-          >
-            Supprimer la recette
-          </button>
+          {user && user.id === recipe.user_id ? (
+            <button
+              type="button"
+              className="delete-recipe-btn"
+              onClick={() => handleDeleteRecipe(recipe.id, navigate)}
+            >
+              Supprimer la recette
+            </button>
+          ) : null}
         </article>
 
         <article className="card-recipe-green">
@@ -136,59 +138,70 @@ export default function RecipesInstruction() {
           <div className="Recette">
             <h2>Recette</h2>
             <div className="Etape-Global">
-              <div className="Etape">
-                <h3>Étape 1 :</h3>
-                <p>
-                  Si besoin, faites décongeler les steaks hachés. Émincez
-                  l’échalote finement.
-                </p>
-              </div>
-              <div className="Etape">
-                <h3>Étape 2 :</h3>
-                <p>
-                  Dans un récipient ajoutez le boeuf, l’oeuf, la chapelure,
-                  l’echalote, la moitié du parmesan, salez, poivrez, malaxez le
-                  tout avec les mains.
-                </p>
-              </div>
-              <div className="Etape">
-                <h3>Étape 3 :</h3>
-                <p>
-                  Faire des boulettes de 4 cm environ en les roulant entre vos
-                  doigts.
-                </p>
-              </div>
-              <div className="Etape">
-                <h3>Étape 4 :</h3>
-                <p>
-                  Cuire les pâtes selon les instructions du paquet dans une eau
-                  salée.
-                </p>
-              </div>
-              <div className="Etape">
-                <h3>Étape 5 :</h3>
-                <p>
-                  Faire revenir les boulettes 5 minutes à la poêle bien chaude
-                  avec un filet d'huile d'olive.
-                </p>
-              </div>
-              <div className="Etape">
-                <h3>Étape 6 :</h3>
-                <p>
-                  En fin de cuisson, baisser le feu au minimum, ajoutez la sauce
-                  tomate, 1 cuillère à soupe d'eau/personne et laissez mijoter
-                  le temps de la prochaine étape.
-                </p>
-              </div>
-              <div className="Etape">
-                <h3>Étape 7 :</h3>
-                <p>
-                  Une fois les pâtes cuites, égouttez-les, puis mélangez-les
-                  avec la sauce et les boulettes. Servir avec un peu de parmesan
-                  (optionnel : et de basilic frais si vous en avez) sur le
-                  dessus. C'est prêt !
-                </p>
-              </div>
+              {recipe.steps && recipe.steps.length !== 0 ? (
+                recipe.steps.map((step) => (
+                  <div className="Etape" key={step.step_number}>
+                    <h3>Étape {step.step_number} :</h3>
+                    <p>{step.description}</p>
+                  </div>
+                ))
+              ) : (
+                <>
+                  <div className="Etape">
+                    <h3>Étape 1 :</h3>
+                    <p>
+                      Si besoin, faites décongeler les steaks hachés. Émincez
+                      l’échalote finement.
+                    </p>
+                  </div>
+                  <div className="Etape">
+                    <h3>Étape 2 :</h3>
+                    <p>
+                      Dans un récipient ajoutez le boeuf, l’oeuf, la chapelure,
+                      l’echalote, la moitié du parmesan, salez, poivrez, malaxez
+                      le tout avec les mains.
+                    </p>
+                  </div>
+                  <div className="Etape">
+                    <h3>Étape 3 :</h3>
+                    <p>
+                      Faire des boulettes de 4 cm environ en les roulant entre
+                      vos doigts.
+                    </p>
+                  </div>
+                  <div className="Etape">
+                    <h3>Étape 4 :</h3>
+                    <p>
+                      Cuire les pâtes selon les instructions du paquet dans une
+                      eau salée.
+                    </p>
+                  </div>
+                  <div className="Etape">
+                    <h3>Étape 5 :</h3>
+                    <p>
+                      Faire revenir les boulettes 5 minutes à la poêle bien
+                      chaude avec un filet d'huile d'olive.
+                    </p>
+                  </div>
+                  <div className="Etape">
+                    <h3>Étape 6 :</h3>
+                    <p>
+                      En fin de cuisson, baisser le feu au minimum, ajoutez la
+                      sauce tomate, 1 cuillère à soupe d'eau/personne et laissez
+                      mijoter le temps de la prochaine étape.
+                    </p>
+                  </div>
+                  <div className="Etape">
+                    <h3>Étape 7 :</h3>
+                    <p>
+                      Une fois les pâtes cuites, égouttez-les, puis mélangez-les
+                      avec la sauce et les boulettes. Servir avec un peu de
+                      parmesan (optionnel : et de basilic frais si vous en avez)
+                      sur le dessus. C'est prêt !
+                    </p>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </article>
